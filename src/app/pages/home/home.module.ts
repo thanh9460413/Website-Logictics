9184b49bd5d5f59f65c02c73a5f0a0028adfd78e
thanh9460413 <hoangthanh9460413@gmail.com>
2024-04-03 17:25:51 +0700

    Update Home Module

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,11 +2,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
-import { NavbarModule } from '../navbar/navbar.module'; // Import NavbarModule
-import { HomeComponent } from './home.component';
+import { NavbarComponent } from '../../components/navbar/navbar'; // Import NavbarModule
+import { HomeComponent } from './home.page';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, NavbarModule], // Thêm NavbarModule vào imports
+  imports: [CommonModule, HomeRoutingModule, NavbarComponent], // Thêm NavbarModule vào imports
 })
 export class HomeModule {}

479072cf2be7761f997c5099ddd4611dfa51a30f
Minh Thang <maiminhthang240902@gmail.com>
2024-04-03 15:00:24 +0700

    Footer & News

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './home.page.html',
-  styleUrl: './home.page.scss'
+  styleUrl: './home.page.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}

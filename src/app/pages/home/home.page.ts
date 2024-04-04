7979a6bce5a3f6f13885fcb8ec864f8e0bb77c49
Minh Thang <maiminhthang240902@gmail.com>
2024-04-04 15:17:14 +0700

    Responsive News section and Footer

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,10 +3,13 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { AboutComponent } from './about/about.page';
 import { WhyUsComponent } from './why-us/why-us.page';
 import { ContactComponent } from './contact/contact.page';
+import { ProjectComponent } from './project/project.page';
+import { NewsComponent } from './news/news.page';
+import { FooterComponent } from '../../components/footer/footer.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, AboutComponent, WhyUsComponent, ContactComponent],
+  imports: [NavbarComponent, AboutComponent, WhyUsComponent,ProjectComponent, ContactComponent, NewsComponent, FooterComponent],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
 })

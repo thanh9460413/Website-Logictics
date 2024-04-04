34f4da1f072f6dffa6a165e390518cde6dfc7f57
Minh Thang <maiminhthang240902@gmail.com>
2024-04-04 15:03:48 +0700

    test

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { AboutComponent } from './about/about.page';
+import { WhyUsComponent } from './why-us/why-us.page';
+import { ContactComponent } from './contact/contact.page';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, AboutComponent, WhyUsComponent, ContactComponent],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
 })

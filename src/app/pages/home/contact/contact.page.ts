7cb9dfe774d5807adf8317df2f63abb8100bf74b
thanh9460413 <hoangthanh9460413@gmail.com>
2024-04-04 10:23:10 +0700

    Create Contact & Update About

---
+++ src/app/pages/home/contact/contact.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact.page.html',
+  styleUrl: './contact.page.scss'
+})
+export class ContactComponent {
+
+}
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { AboutComponent } from '../about/about.page';
-import { WhyUsComponent } from '../why-us/why-us.page';
+import { AboutComponent } from './about/about.page';
+import { WhyUsComponent } from './why-us/why-us.page';
 import { FooterComponent } from '../../components/footer/footer.component';
-import { ProjectComponent } from '../project/project.page';
-import { NewsComponent } from '../news/news.page';
+import { ProjectComponent } from './project/project.page';
+import { NewsComponent } from './news/news.page';
+import { ContactComponent } from './contact/contact.page';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,AboutComponent,WhyUsComponent,FooterComponent,ProjectComponent,NewsComponent],
+  imports: [NavbarComponent,AboutComponent,WhyUsComponent,FooterComponent,ProjectComponent,NewsComponent,ContactComponent],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
